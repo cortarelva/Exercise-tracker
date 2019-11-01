16f4a5411a808a91b16e023f92d6879222dedde0
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-11-01 22:13:56 +0000

    functional first part done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from '../src/components/navbar.component';
+import CreateExercise from '../src/components/create-exercise.component';
+import CreateUser from '../src/components/create-user.component';
+import EditExercise from '../src/components/edit-exercise.component';
+import ExercisesList from '../src/components/exercises-list.component';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="container">
+          <Navbar />
+          <br/>
+          <Route path="/" exact component={ExercisesList} />
+          <Route path="/edit/:id" component={EditExercise} />
+          <Route path="/create" component={CreateExercise} />
+          <Route path="/user" component={CreateUser} />
     </div>
+    </Router>
   );
 }
 
